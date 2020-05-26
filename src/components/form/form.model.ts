5e55d9dc03c2dcaae844c1910ee825ccb445cc5a
David Waller <dwaller@apple.com>
2020-05-26 06:42:04 +0200

    model form to use reducers throughout

--- src/components/form/form.model.ts
+++ src/components/form/form.model.ts
@@ -1,21 +1,15 @@
-import xs, { Stream } from "xstream";
-import { FormState, FormAction, FormSources } from "./form.types";
+import { Stream } from "xstream";
+import { FormState, FormAction } from "./form.types";
+import { Reducer } from "@cycle/state";
 export * from "./form.types";
 
-export function model(
-  sources: FormSources,
-  action$: Stream<FormAction>
-): Stream<FormState> {
-  const state$: Stream<FormState> = xs
-    .merge(
-      sources.state.stream,
-      action$.map((action) => ({
-        submittedName: action,
-        fieldContent: "",
-      }))
-    )
-    .startWith({ submittedName: "", fieldContent: "" });
-  return state$;
+export function model(action$: Stream<FormAction>): Stream<Reducer<FormState>> {
+  return action$
+    .map((action) => (s: FormState) => ({
+      submittedName: action,
+      fieldContent: "",
+    }))
+    .startWith(() => ({ submittedName: "", fieldContent: "" }));
 }
 
 export default model;
--- src/components/form/form.ts
+++ src/components/form/form.ts
@@ -1,13 +1,10 @@
-import xs, { Stream } from "xstream";
-import { Reducer } from "@cycle/state";
-
+import xs from "xstream";
 import intent from "./form.intent";
 import view from "./form.view";
 import model from "./form.model";
 import useConfirmButton from "./form.useConfirmButton";
 import useAssignableInput from "./form.useAssignableInput";
-import { FormSinks, FormSources, FormState } from "./form.types";
-export * from "./form.types";
+import { FormSinks, FormSources } from "./form.types";
 
 export function Form(sources: FormSources): FormSinks {
   const assignableInputSinks = useAssignableInput(sources);
@@ -15,20 +12,11 @@ export function Form(sources: FormSources): FormSinks {
 
   const action$ = intent(sources, confirmButtonSinks.submit$);
   const vtree$ = view(assignableInputSinks.DOM, confirmButtonSinks.DOM);
-  const model$ = model(sources, action$);
-
-  const defaultReducer$: Stream<Reducer<FormState>> = xs.of(
-    (s) => s || { submittedName: "", fieldContent: "" }
-  );
-  const submitReducer$: Stream<Reducer<FormState>> = model$.map((s) => () => s);
+  const reducer$ = model(action$);
 
   return {
     DOM: vtree$,
-    state: xs.merge(
-      defaultReducer$,
-      assignableInputSinks.state,
-      submitReducer$
-    ),
+    state: xs.merge(reducer$, assignableInputSinks.state),
   };
 }
 
